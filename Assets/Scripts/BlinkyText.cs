f4967013314330653efb8fbff1565b9212208459
Scott Wise <scottw@unity3d.com>
2019-01-26 22:36:54 -0800

    Main menu scene

---
+++ Assets/Scripts/BlinkyText.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BlinkyText : MonoBehaviour
+{
+    public TextMeshProUGUI text;
+    
+    void Start()
+    {
+        StartBlinking();
+    }
+
+    private IEnumerator Blink()
+    {
+        while (text)
+        {
+            switch (text.color.a.ToString())
+            {
+                case "0":
+                    text.color = new Color(text.color.r, text.color.b, text.color.g, 1);
+                    yield return new WaitForSeconds(0.5f);
+                    break;
+                case "1":
+                    text.color = new Color(text.color.r, text.color.b, text.color.g, 0);
+                    yield return new WaitForSeconds(0.4f);
+                    break;
+            }
+        }
+    }
+
+    void StartBlinking()
+    {
+        StopCoroutine("Blink");
+        StartCoroutine("Blink");
+    }
+}
---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            LoadScene();
+        }
+    }
+    
+    public void LoadScene()
+    {
+        SceneManager.LoadScene("Main");
+    }
+}
